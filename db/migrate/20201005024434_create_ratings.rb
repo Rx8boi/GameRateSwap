4e9ba2b14d4d6b678b3e50b2ecfd42d20b96364b
Rx8boi <60455199+Rx8boi@users.noreply.github.com>
2020-10-03 15:32:12 -0500

    created ratings table

--- db/migrate/20201005024434_create_ratings.rb
+++ db/migrate/20201005024434_create_ratings.rb
@@ -1,6 +1,11 @@
 class CreateRatings < ActiveRecord::Migration[6.0]
   def change
     create_table :ratings do |t|
+    	t.integer "stars"
+    	t.string "difficulty"
+    	t.integer "game_id"
+    	t.integer "user_id"
+    	t.datetime "reviewed_at"
     end
   end
 end
