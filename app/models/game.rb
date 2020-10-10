06a717f8926096871fb51881df78f66d7698a6e4
Rx8boi <60455199+Rx8boi@users.noreply.github.com>
2020-10-10 16:24:46 -0500

    Fixed database error

---
+++ app/models/game.rb
@@ -0,0 +1,6 @@
+class Game < ApplicationRecord
+	belongs_to :platform
+	belongs_to :user
+	has_many :ratings
+	has_many :users, through: :ratings
+end
---
+++ db/migrate/20201004011952_create_ratings.rb
@@ -0,0 +1,12 @@
+class CreateRatings < ActiveRecord::Migration[6.0]
+  def change
+    create_table :ratings do |t|
+      t.integer :stars
+      t.string :difficulty
+      t.integer :game_id
+      t.integer :user_id
+
+      t.timestamps
+    end
+  end
+end
