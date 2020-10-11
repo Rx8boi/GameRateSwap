6a49feaed3d326af02ed44b4b595ee97be4257f7
Rx8boi <60455199+Rx8boi@users.noreply.github.com>
2020-10-10 21:21:29 -0500

    configured devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+
+
   resources :users
   resources :games
   resources :ratings
   resources :platform
+  root 'home#index'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
