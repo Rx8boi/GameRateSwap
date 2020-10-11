f15ab2c1830157e132f4a97f617af8f9ca1c3259
Rx8boi <60455199+Rx8boi@users.noreply.github.com>
2020-10-10 21:43:39 -0500

    Devise User model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_04_011952) do
+ActiveRecord::Schema.define(version: 2020_10_11_022940) do
 
   create_table "games", force: :cascade do |t|
     t.string "title"
@@ -41,6 +41,13 @@ ActiveRecord::Schema.define(version: 2020_10_04_011952) do
     t.string "username"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
