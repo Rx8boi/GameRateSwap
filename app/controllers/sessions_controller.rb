c80527bceae44d6d11a13e03133f05a9e5174388
Rx8boi <60455199+Rx8boi@users.noreply.github.com>
2020-10-12 17:14:28 -0500

    adding controllers
    added sessions controller and users

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,12 @@
+class SessionsController < ApplicationRecord
+
+	def new
+	end
+
+	def create
+	end
+
+	def home
+	end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,16 @@
 Rails.application.routes.draw do
-
+	
+	root 'home#index'
+	get '/signup' => 'user#new'
+	get '/login' => 'sessions#new'
+	post '/login' => 'session#create'
 
   devise_for :users
   resources :users
   resources :games
   resources :ratings
   resources :platform
-  root 'home#index'
+  
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
