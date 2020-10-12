54af88b92a765903923f918ace803a7d2fd14762
Rx8boi <60455199+Rx8boi@users.noreply.github.com>
2020-10-12 18:55:00 -0500

    Update users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,21 @@ class UsersController < ApplicationController
 
 #method to handle signup
 	def create
+		@user = User.new(user_params)
+		if @user.save
+		#login the user
+		session[:user_id] = @user.user_id
+		redirect_to :home
+		else
+			render :signup
+		end
+
+	end
+
+	private
+
+	def user_params
+		params.require(:user).permit( :email, :encrypted_password)
 	end
 
 end
