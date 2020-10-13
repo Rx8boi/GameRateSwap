b5cce95fa09b93014eb3b1aa9a37043b1ac9d37b
Rx8boi <60455199+Rx8boi@users.noreply.github.com>
2020-10-13 10:21:03 -0500

    updated menu navigation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,28 +2,36 @@ class UsersController < ApplicationController
 
 #method for loading signup form
   def new
+
   end
 
   def create
     @user = User.new(user_params)
     if @user.save
       session[:user_id] = @user.id
-      redirect_to user_path(@user)
+      redirect_to users_path
     else
       render :new
     end
   end
 
-  def show
-    redirect_if_not_logged_in
+  def index
+    if user_signed_in?
     @user = User.find_by_id(params[:id])
-    redirect_to '/' if !@user
+    else
+    	redirect_to '/'
+    end
   end
+
+  def show
+  	@user = User.last
+  	 redirect_to users_path
+  	end
 
   private
 
-  def user_params
-    params.require(:user).permit(:username, :email, :password)
-  end
+  #def user_params
+   # params.require(:user).permit(:username, :email, :password)
+  #end
 
 end
