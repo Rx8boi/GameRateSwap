444913d0f1f5ed462c5712c2a8b00fb3cf4450f8
Rx8boi <60455199+Rx8boi@users.noreply.github.com>
2020-10-14 18:04:48 -0500

    added duplication error

--- app/models/game.rb
+++ app/models/game.rb
@@ -5,11 +5,20 @@ class Game < ApplicationRecord
 	has_many :users, through: :ratings
 	#has_one_attached :image
 	accepts_nested_attributes_for :platform
+
 	validates :title, presence: true
-	#validate :not_a_duplicate
+	validate :not_duplicate
 
 	def new
 		@game = Game.new
 	end
+
+
+	def not_duplicate
+		if Game.find_by(flavor: flavor, brand_id: brand_id)
+	end
+#game needs to be unique when added
+
+
 
 end
