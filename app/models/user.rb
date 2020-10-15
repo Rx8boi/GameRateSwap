14c1c7fc0cb691d39ee48f9a53ba596e07fc2899
Rx8boi <60455199+Rx8boi@users.noreply.github.com>
2020-10-15 06:11:01 -0500

    omniauth controller & google auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,20 @@
-class User < ApplicationRecord
+ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         :omniauthable, omniauth_providers: [:google_oauth2]
 
 	has_many :ratings
 	has_many :rated_games, through: :ratings, source: :game
 
 	#but also user has created many games to the list
 	has_many :games 
+
+	def self.create_from_provider_data(provider_data)
+		where(provider: provider_data.provider, uid: provider_data.uid).first_or_create do |user|
+			user.email = provider_data.info.email
+			user.password =Devise.friendly_token[0, 20]
+		end
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-	 devise_for :users
+	 devise_for :users, controllers: {omniauth_callbacks: 'omniauth'}
 	root 'home#index'
 
   resources :users
@@ -9,7 +9,6 @@ Rails.application.routes.draw do
   resources :ratings
   resources :platforms
 
- get '/auth/:provider/callback' => 'sessions#omniauth'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
